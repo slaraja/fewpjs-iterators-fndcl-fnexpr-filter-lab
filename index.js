866b4f46c89929bb9549e312eae35bf04f776b8a
Stephanie La Raja <stephanielaraja@gmail.com>
2021-08-01 10:29:25 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+//example:
+// let even = arr.filter(n => {
+//     return n % 2 === 0;
+//   });
+//   // even = [2,4,6] 
+  
+
+
+//This function takes an array of drivers and a string as arguments
+//and returns the matching list of drivers. 
+//The function should be case insensitive.
+
+function findMatching(drivers, str) {
+    return drivers.filter (driver => driver.toLowerCase() === str.toLowerCase())
+}
+
+function fuzzyMatch(drivers, str) {
+    return drivers.filter (driver => driver.slice(0, str.length) === str)
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(driver => driver.name === string)
+  }
